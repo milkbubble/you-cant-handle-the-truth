58b4433ea7d31d34de325b10567d1cbe6d503b6d
milkbubble <chagga@uw.edu>
2020-11-21 20:28:37 -0800

    first string added

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,26 @@
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount();
+        String stringTestOne = "Hi my name is Tim Medvedev and today I am testing a " +
+                             " word count method to see how many words I can count";
+
+        String[] testCase1 = stringTestOne.split(" ");
+        String[] testCase2 = stringTestTwo.split(" ");
+        String[] testCase3 = stringTestThree.split(" ");
+        String[] testCase4 = stringTestFour.split(" ");
+        String[] testCase5 = stringTestFive.split(" ");
+        String[][] testCases = new String[][] { testCase1, testCase2, testCase3, testCase4, testCase5};
+
+        for (int i = 0; i < testCases.length; i ++) {
+            for (int j = 0; j < testCases[i].length;) {
+                assertEquals(HandleTruth.wordCount(testCases[i]), expected.get(i));
+            }
+        }
     }
 }
